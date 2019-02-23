3ba67437b613fc9bb8f47c3d846022eeb7ac4d77
Daniyar Kurmankhojayev <kurman.daniyar@gmail.com>
2019-02-23 14:44:29 +0600

    form segmentation code

---
+++ src/form_segmentation/misc.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+
+def get_file_names(path):
+	"""
+	Input: path to the directory
+	Output: list of files
+	"""
+	fnames = []
+	if os.path.exists(path):
+		fnames = os.listdir(path)
+	else:
+		print ('Directory does not exist: ', path)
+	fnames.sort()
+	return fnames
