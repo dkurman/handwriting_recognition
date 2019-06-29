3dc739bc3081f02a14f9025d0fce9fc47a43632f
Zhuldyzzhan Sagimbayev <cruigo93@gmail.com>
2019-06-29 14:37:35 +0600

    big files

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask, request
 from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+import os
 
 app = Flask(__name__)
 dropzone = Dropzone(app)
+
+app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'index'
+
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
 
 from backend import routes
--- src/backend/routes.py
+++ src/backend/routes.py
@@ -1,19 +1,46 @@
-from backend import app, dropzone, request
+from backend import app, dropzone, request, photos
 from flask import render_template
 import os
+from flask import Flask, redirect, render_template, request, session, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    # redirect to home if no images to display
+    if "file_urls" not in session or session['file_urls'] == []:
+        return redirect(url_for('index'))
+        
+    # set the file_urls and remove the session variable
+    file_urls = session['file_urls']
+    session.pop('file_urls', None)
+    
+    return render_template('res.html', file_urls=file_urls)
 
 
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload():
 
+    # set session for image results
+    if "file_urls" not in session:
+        session['file_urls'] = []
+    # list to hold our uploaded image urls
+    file_urls = session['file_urls']
+    # handle image upload from Dropzone
     if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join('static', f.filename))
-
-    return 'upload template'
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+            
+            # save the file with to our photos folder
+            filename = photos.save(
+                file,
+                name=file.filename    
+            )
+            # append image urls
+            file_urls.append(photos.url(filename))
+            
+        session['file_urls'] = file_urls
+        return "uploading..."
+    # return dropzone template on GET request    
+    return render_template('index.html')
